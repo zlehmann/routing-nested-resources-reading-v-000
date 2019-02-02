77a9066015d731c9e2d6daf038fdcf6e9f0e9136
Zachary Lehmann <zlehmann@gmail.com>
2019-02-02 22:39:50 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,9 @@ class PostsController < ApplicationController
   def index
     if params[:author_id]
       @posts = Author.find(params[:author_id]).posts
-    @posts = Post.all
+    else
+      @posts = Post.all
+    end
   end
 
   def show
